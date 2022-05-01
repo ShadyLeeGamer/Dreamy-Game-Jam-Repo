bc958416dea62908f43ad0e9455bc01bf5e02bb1
BloodCryHiro <jo090989coding@gmail.com>
2022-05-01 21:41:50 +0800

    Add ground check method to player controller

--- Dreamy_Game_Jam/Assets/PlayerController.cs
+++ Dreamy_Game_Jam/Assets/PlayerController.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private BoxCollider2D boxCol;
+
     private Vector2 velocity;
     [SerializeField] private float moveSpeed;
+
+    private bool onGround = false;
     private bool jump = false;
     [SerializeField] private float jumpForce;
+    [SerializeField] private LayerMask platformMask;
 
     private void Update()
     {
         GetMovement();
+        CheckOnGround();
     }
 
     private void FixedUpdate()
@@ -30,7 +34,15 @@ public class PlayerController : MonoBehaviour
     {
         velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
             jump = true;
+    }
+
+    private void CheckOnGround()
+    {
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y);
+        RaycastHit2D hit = Physics2D.BoxCast(origin, boxCol.size, 0f, Vector2.down, 0.05f, platformMask);
+
+        onGround = hit.collider != null;
     }
 }
